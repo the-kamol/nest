d55893dd25bf8f1ebc3df8f0c7224a6f31a4b1ac
abdujamilov <vip.abdujamilov@gmail.com>
2025-04-27 15:27:26 +0500

    catch added to bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (err) {
+    console.log(`error`, err);
+  }
 }
-bootstrap();
+
+bootstrap().catch(console.log);
